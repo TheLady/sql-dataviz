971bcec6e5e2851b78d96f1231c76fe1a7fcf82e
Helen Yau <helenyau13@gmail.com>
2017-01-20 11:00:24 -0800

    fix syntax for tweets-mini.js

--- data/tweets-mini.js
+++ data/tweets-mini.js
@@ -2,364 +2,363 @@
   {
     "tweet_id": 821583224790884000,
     "text": "words1",
-    "expanded_urls": "https://www.thenation.com/article/long-list-what-we-know-thanks-private-manning/",
-    "": "CREATE TABLE tweets-test (id SERIAL PRIMARY KEY, text varchar(140), expanded_urls varchar(140));"
+    "expanded_urls": "https://www.thenation.com/article/long-list-what-we-know-thanks-private-manning/"
+
   },
   {
     "tweet_id": 821581559354405000,
     "text": "words2",
-    "expanded_urls": "",
-    "": ""
+    "expanded_urls": ""
+
   },
   {
     "tweet_id": 820905434043191000,
     "text": "words3",
-    "expanded_urls": "https://twitter.com/tenderlove/status/820084722453778432",
-    "": ""
+    "expanded_urls": "https://twitter.com/tenderlove/status/820084722453778432"
+
   },
   {
     "tweet_id": 819715440817545000,
     "text": "words4",
-    "expanded_urls": "https://twitter.com/Wikipedia/status/819300078665879552/video/1",
-    "": ""
+    "expanded_urls": "https://twitter.com/Wikipedia/status/819300078665879552/video/1"
+
   },
   {
     "tweet_id": 817955914346942000,
     "text": "words5",
-    "expanded_urls": "",
-    "": ""
+    "expanded_urls": ""
+
   },
   {
     "tweet_id": 817952080698569000,
     "text": "words6",
-    "expanded_urls": "2017-01-05 03:39:00 +0000",
-    "": ""
+    "expanded_urls": "2017-01-05 03:39:00 +0000"
+
   },
   {
     "tweet_id": 817630439426793000,
     "text": "words7",
-    "expanded_urls": "https://apple.news/AImgMtfGiRTWm6dgKkjdj5w",
-    "": ""
+    "expanded_urls": "https://apple.news/AImgMtfGiRTWm6dgKkjdj5w"
+
   },
   {
     "tweet_id": 817630317737456000,
     "text": "words8",
-    "expanded_urls": "https://namebadgeproductions.com/pages/recyclingProgram",
-    "": ""
+    "expanded_urls": "https://namebadgeproductions.com/pages/recyclingProgram"
+
   },
   {
     "tweet_id": 817630292005372000,
     "text": "words9",
-    "expanded_urls": "",
-    "": ""
+    "expanded_urls": ""
+
   },
   {
     "tweet_id": 817263003011727000,
     "text": "words10",
-    "expanded_urls": "",
-    "": ""
+    "expanded_urls": ""
+
   },
   {
     "tweet_id": 817259603213750000,
     "text": "words11",
-    "expanded_urls": "https://osf.to/2iaKh2A",
-    "": ""
+    "expanded_urls": "https://osf.to/2iaKh2A"
+
   },
   {
     "tweet_id": 816562209526419000,
     "text": "words12",
-    "expanded_urls": "",
-    "": ""
+    "expanded_urls": ""
+
   },
   {
     "tweet_id": 815621999145853000,
     "text": "words13",
-    "expanded_urls": "http://www.nytimes.com/2016/12/27/magazine/finding-inspiration-for-art-in-the-betrayal-of-privacy.html",
-    "": ""
+    "expanded_urls": "http://www.nytimes.com/2016/12/27/magazine/finding-inspiration-for-art-in-the-betrayal-of-privacy.html"
   },
   {
     "tweet_id": 815621084397510000,
     "text": "words14",
-    "expanded_urls": "https://twitter.com/Scheeatow/status/815570994962321408/photo/1",
-    "": ""
+    "expanded_urls": "https://twitter.com/Scheeatow/status/815570994962321408/photo/1"
+
   },
   {
     "tweet_id": 815489062098366000,
     "text": "words15",
-    "expanded_urls": "",
-    "": ""
+    "expanded_urls": ""
+
   },
   {
     "tweet_id": 813649927683224000,
     "text": "words16",
-    "expanded_urls": "http://www.hongkiat.com/blog/bitcoin-wallets/",
-    "": ""
+    "expanded_urls": "http://www.hongkiat.com/blog/bitcoin-wallets/"
+
   },
   {
     "tweet_id": 813648849591890000,
     "text": "words17",
-    "expanded_urls": "",
-    "": ""
+    "expanded_urls": ""
+
   },
   {
     "tweet_id": 813647664654885000,
     "text": "words18",
-    "expanded_urls": "",
-    "": ""
+    "expanded_urls": ""
+
   },
   {
     "tweet_id": 813494194333479000,
     "text": "words19",
-    "expanded_urls": "",
-    "": ""
+    "expanded_urls": ""
+
   },
   {
     "tweet_id": 813491217967116000,
     "text": "words20",
-    "expanded_urls": "",
-    "": ""
+    "expanded_urls": ""
+
   },
   {
     "tweet_id": 812587066282234000,
     "text": "words21",
-    "expanded_urls": "",
-    "": ""
+    "expanded_urls": ""
+
   },
   {
     "tweet_id": 812128630503600000,
     "text": "words22",
-    "expanded_urls": "",
-    "": ""
+    "expanded_urls": ""
+
   },
   {
     "tweet_id": 811480670010605000,
     "text": "words23",
-    "expanded_urls": "https://twitter.com/MarsWxReport/status/809473385768095744/photo/1",
-    "": ""
+    "expanded_urls": "https://twitter.com/MarsWxReport/status/809473385768095744/photo/1"
+
   },
   {
     "tweet_id": 809671025138942000,
     "text": "words24",
-    "expanded_urls": "https://www.youtube.com/watch?v=ElJhoGzTnXU",
-    "": ""
+    "expanded_urls": "https://www.youtube.com/watch?v=ElJhoGzTnXU"
+
   },
   {
     "tweet_id": 809379346355888000,
     "text": "words25",
-    "expanded_urls": "http://imgur.com/gallery/1KytDTQ",
-    "": ""
+    "expanded_urls": "http://imgur.com/gallery/1KytDTQ"
+
   },
   {
     "tweet_id": 808941543046267000,
     "text": "words26",
-    "expanded_urls": "https://www.youtube.com/watch?v=ecT42O6I_WI",
-    "": ""
+    "expanded_urls": "https://www.youtube.com/watch?v=ecT42O6I_WI"
+
   },
   {
     "tweet_id": 808941472024170000,
     "text": "words27",
-    "expanded_urls": "2016-12-14 04:42:02 +0000",
-    "": ""
+    "expanded_urls": "2016-12-14 04:42:02 +0000"
+
   },
   {
     "tweet_id": 808937291414549000,
     "text": "words28",
-    "expanded_urls": "https://twitter.com/elonmusk/status/807354766804168706/video/1",
-    "": ""
+    "expanded_urls": "https://twitter.com/elonmusk/status/807354766804168706/video/1"
+
   },
   {
     "tweet_id": 808910904087679000,
     "text": "words29",
-    "expanded_urls": "",
-    "": ""
+    "expanded_urls": ""
+
   },
   {
     "tweet_id": 807071940921868000,
     "text": "words30",
-    "expanded_urls": "https://twitter.com/NASA/status/806960800669794305",
-    "": ""
+    "expanded_urls": "https://twitter.com/NASA/status/806960800669794305"
+
   },
   {
     "tweet_id": 807071727557624000,
     "text": "words31",
-    "expanded_urls": "2016-12-08 23:50:26 +0000",
-    "": ""
+    "expanded_urls": "2016-12-08 23:50:26 +0000"
+
   },
   {
     "tweet_id": 806757965193613000,
     "text": "words32",
-    "expanded_urls": "2016-12-07 23:49:55 +0000",
-    "": ""
+    "expanded_urls": "2016-12-07 23:49:55 +0000"
+
   },
   {
     "tweet_id": 806343774477681000,
     "text": "words33",
-    "expanded_urls": "",
-    "": ""
+    "expanded_urls": ""
+
   },
   {
     "tweet_id": 806037061178458000,
     "text": "words34",
-    "expanded_urls": "https://twitter.com/dihaynes/status/806037061178458112/photo/1",
-    "": ""
+    "expanded_urls": "https://twitter.com/dihaynes/status/806037061178458112/photo/1"
+
   },
   {
     "tweet_id": 805640731213763000,
     "text": "words35",
-    "expanded_urls": "2016-12-04 03:17:58 +0000",
-    "": ""
+    "expanded_urls": "2016-12-04 03:17:58 +0000"
+
   },
   {
     "tweet_id": 805640555266879000,
     "text": "words36",
-    "expanded_urls": "https://twitter.com/Shonna_Brown/status/805403218830356480/photo/1",
-    "": ""
+    "expanded_urls": "https://twitter.com/Shonna_Brown/status/805403218830356480/photo/1"
+
   },
   {
     "tweet_id": 805635451461608000,
     "text": "words37",
-    "expanded_urls": "",
-    "": ""
+    "expanded_urls": ""
+
   },
   {
     "tweet_id": 805598559546458000,
     "text": "words38",
-    "expanded_urls": "http://www.theroot.com/articles/culture/2016/12/nodapl-big-win-at-standing-rock-as-army-corps-denies-easement-for-dakota-access-pipeline/",
-    "": ""
+    "expanded_urls": "http://www.theroot.com/articles/culture/2016/12/nodapl-big-win-at-standing-rock-as-army-corps-denies-easement-for-dakota-access-pipeline/"
+
   },
   {
     "tweet_id": 805597611021332000,
     "text": "words39",
-    "expanded_urls": "https://twitter.com/mattblaze/status/805499913106759684",
-    "": ""
+    "expanded_urls": "https://twitter.com/mattblaze/status/805499913106759684"
+
   },
   {
     "tweet_id": 805596433600516000,
     "text": "words40",
-    "expanded_urls": "http://www.nytimes.com/2016/12/02/opinion/fake-cowboys-and-real-indians.html",
-    "": ""
+    "expanded_urls": "http://www.nytimes.com/2016/12/02/opinion/fake-cowboys-and-real-indians.html"
+
   },
   {
     "tweet_id": 805595999691424000,
     "text": "words41",
-    "expanded_urls": "https://twitter.com/ChuckModi1/status/805442314810363904/video/1",
-    "": ""
+    "expanded_urls": "https://twitter.com/ChuckModi1/status/805442314810363904/video/1"
+
   },
   {
     "tweet_id": 805595276492743000,
     "text": "words42",
-    "expanded_urls": "",
-    "": ""
+    "expanded_urls": ""
+
   },
   {
     "tweet_id": 805589248967672000,
     "text": "words43",
-    "expanded_urls": "",
-    "": ""
+    "expanded_urls": ""
+
   },
   {
     "tweet_id": 804938765043048000,
     "text": "words44",
-    "expanded_urls": "",
-    "": ""
+    "expanded_urls": ""
+
   },
   {
     "tweet_id": 804234902816178000,
     "text": "words45",
-    "expanded_urls": "http://www.azquotes.com/quote/1057990",
-    "": ""
+    "expanded_urls": "http://www.azquotes.com/quote/1057990"
+
   },
   {
     "tweet_id": 804233504242352000,
     "text": "words46",
-    "expanded_urls": "http://www.azquotes.com/quote/1137518",
-    "": ""
+    "expanded_urls": "http://www.azquotes.com/quote/1137518"
+
   },
   {
     "tweet_id": 804232646662328000,
     "text": "words47",
-    "expanded_urls": "2016-11-30 15:48:17 +0000",
-    "": ""
+    "expanded_urls": "2016-11-30 15:48:17 +0000"
+
   },
   {
     "tweet_id": 804229702000877000,
     "text": "words48",
-    "expanded_urls": "2016-11-30 20:35:36 +0000",
-    "": ""
+    "expanded_urls": "2016-11-30 20:35:36 +0000"
+
   },
   {
     "tweet_id": 803851514024432000,
     "text": "words49",
-    "expanded_urls": "",
-    "": ""
+    "expanded_urls": ""
+
   },
   {
     "tweet_id": 803073646126244000,
     "text": "words50",
-    "expanded_urls": "",
-    "": ""
+    "expanded_urls": ""
+
   },
   {
     "tweet_id": 802404006718886000,
     "text": "words51",
-    "expanded_urls": "http://bit.ly/2gHvQ5g",
-    "": ""
+    "expanded_urls": "http://bit.ly/2gHvQ5g"
+
   },
   {
     "tweet_id": 802403679810646000,
     "text": "words52",
-    "expanded_urls": 807095,
-    "": ""
+    "expanded_urls": 807095
+
   },
   {
     "tweet_id": 802067941881586000,
     "text": "words53",
-    "expanded_urls": "http://ow.ly/U9hMR",
-    "": ""
+    "expanded_urls": "http://ow.ly/U9hMR"
+
   },
   {
     "tweet_id": 801908035723498000,
     "text": "words54",
-    "expanded_urls": "https://www.amazon.com/gp/aw/ls/ref=cm_sw_r_cp_ep_ws_9qRnybRQHT9F1?ie=UTF8&lid=18FR1AGDPWZLC&ref_=cm_sw_r_cp_ep_ws_9qRnybRQHT9F1&ty=wishlist",
-    "": ""
+    "expanded_urls": "https://www.amazon.com/gp/aw/ls/ref=cm_sw_r_cp_ep_ws_9qRnybRQHT9F1?ie=UTF8&lid=18FR1AGDPWZLC&ref_=cm_sw_r_cp_ep_ws_9qRnybRQHT9F1&ty=wishlist"
+
   },
   {
     "tweet_id": 801907166403695000,
     "text": "words55",
-    "expanded_urls": "",
-    "": ""
+    "expanded_urls": ""
+
   },
   {
     "tweet_id": 801895210242314000,
     "text": "words56",
-    "expanded_urls": "https://twitter.com/AGSchneiderman/status/801821622114156544",
-    "": ""
+    "expanded_urls": "https://twitter.com/AGSchneiderman/status/801821622114156544"
+
   },
   {
     "tweet_id": 801894903223464000,
     "text": "words57",
-    "expanded_urls": "2016-11-24 15:45:01 +0000",
-    "": ""
+    "expanded_urls": "2016-11-24 15:45:01 +0000"
+
   },
   {
     "tweet_id": 801890888091742000,
     "text": "words58",
-    "expanded_urls": 13145012,
-    "": ""
+    "expanded_urls": 13145012
+
   },
   {
     "tweet_id": 801875706988806000,
     "text": "words59",
-    "expanded_urls": "https://riseup.net/en/canary",
-    "": ""
+    "expanded_urls": "https://riseup.net/en/canary"
+
   },
   {
     "tweet_id": 801334251903942000,
     "text": "words60",
-    "expanded_urls": "http://snpy.tv/2foB6cb",
-    "": ""
+    "expanded_urls": "http://snpy.tv/2foB6cb"
+
   },
   {
     "tweet_id": 801334216042676000,
     "text": "words61",
-    "expanded_urls": "http://brianfor.de/1Qoy4h4",
+    "expanded_urls": "http://brianfor.de/1Qoy4h4"
