726e38d97c35fec6f8df72ed3814a781d62378dc
Helen Yau <helenyau13@gmail.com>
2017-01-20 16:53:18 -0800

    insert more values to tweets table

--- schema.sql
+++ schema.sql
@@ -17,4 +17,7 @@ INSERT INTO
 VALUES
   (821581559354405000,'words2');
 
-  
+  INSERT INTO
+    tweets (tweets_id, text, expanded_urls)
+  VALUES
+    (820905434043191000,'words3', 'https://twitter.com/Wikipedia/status/819300078665879552/video/1');
