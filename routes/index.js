2f859806a55888189683fd2994646f5a729ddff9
Helen Yau <helenyau13@gmail.com>
2017-01-20 16:22:50 -0800

    Connect database to server using Express

--- routes/index.js
+++ routes/index.js
@@ -1,36 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const pg = require('pg');
-const path = require('path');
-const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/sqldv';
+const database = require('../database');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'SQL DATA VIZ!!!' });
 });
 
-router.post('/api/v1/sqldv', (req, res, next) => {
-  const results = [];
-  const data = {text: req.body.text, expanded_urls};
-  pg.connect(connectionString, (err, client, done) => {
-    if(err) {
-      done();
-      console.log(err);
-      return res.status(500).json({success: false, data: err});
-    }
-    client.query('INSERT INTO tweets (text, expanded_urls) values(word1, https://www.thenation.com/article/long-list-what-we-know-thanks-private-manning/)', [data.text, data.expanded_urls]);
-    const query = client.query('SELECT * FROM tweets ORDER BY id ASC');
-    query.on('row', (row) => {
-      results.push(row);
-    });
-    query.on('end', () => {
-      done();
-      return res.json(results);
-    });
+router.get('/tweets', (req, res, next) => {
+  database.query('SELECT * FROM tweets', (error, result) => {
+    if (error) return res.status(500).json({error: error})
+    res.json(result.rows)
   })
 })
 
 
 module.exports = router;
-
-
--- schema.sql
+++ schema.sql
@@ -1,5 +1,20 @@
-CREATE TABLE people (
+DROP TABLE tweets;
+
+CREATE TABLE IF NOT EXISTS tweets (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(255),
-  birthday date
-)
+  tweets_id BIGINT,
+  text VARCHAR(255),
+  expanded_urls VARCHAR
+);
+
+INSERT INTO
+  tweets (tweets_id, text, expanded_urls)
+VALUES
+  (821583224790884000, 'words1', 'https://www.thenation.com/article/long-list-what-we-know-thanks-private-manning/');
+
+INSERT INTO
+  tweets (tweets_id, text)
+VALUES
+  (821581559354405000,'words2');
+
+  
